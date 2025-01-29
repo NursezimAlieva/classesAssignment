42c1fd5bd13923b113f48d100213fd8471244b1b
NursezimAlieva <nursezim.alieva@alatoo.edu.kg>
2025-01-29 22:10:46 +0600

    Update main.py

--- main.py
+++ main.py
@@ -2,5 +2,11 @@ from book import Book
 
 if __name__ == '__main__':
 
-    person1 =Book('1984', 'George Orwell', ISBN='978-5-17-080115-2')
-    person1.info()
+    # Creating a Book object
+    book1 = Book(title="The Great Gatsby", author="F. Scott Fitzgerald", ISBN="1234567890", price=10.99)
+
+    # Getting the information about the book
+    book1.info()
+
+    # Selling the book
+    book1.sell()
